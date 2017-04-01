3516edf8046678db1290c3df2e574c95a2a9fc07
Chris Pillion <christopherpillion@gmail.com>
2017-04-01 12:18:25 -0600

    Cleaned ogl4setup dir

---
+++ ogl4Setup/moc_CUgl.cpp
@@ -0,0 +1,234 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'CUgl.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.7.1)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../Project/CUgl.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'CUgl.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.7.1. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+struct qt_meta_stringdata_CUgl_t {
+    QByteArrayData data[35];
+    char stringdata0[250];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_CUgl_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_CUgl_t qt_meta_stringdata_CUgl = {
+    {
+QT_MOC_LITERAL(0, 0, 4), // "CUgl"
+QT_MOC_LITERAL(1, 5, 5), // "light"
+QT_MOC_LITERAL(2, 11, 0), // ""
+QT_MOC_LITERAL(3, 12, 5), // "angle"
+QT_MOC_LITERAL(4, 18, 6), // "angles"
+QT_MOC_LITERAL(5, 25, 4), // "text"
+QT_MOC_LITERAL(6, 30, 5), // "reset"
+QT_MOC_LITERAL(7, 36, 6), // "setDim"
+QT_MOC_LITERAL(8, 43, 1), // "d"
+QT_MOC_LITERAL(9, 45, 14), // "setPerspective"
+QT_MOC_LITERAL(10, 60, 2), // "on"
+QT_MOC_LITERAL(11, 63, 9), // "setShader"
+QT_MOC_LITERAL(12, 73, 3), // "sel"
+QT_MOC_LITERAL(13, 77, 9), // "setObject"
+QT_MOC_LITERAL(14, 87, 4), // "type"
+QT_MOC_LITERAL(15, 92, 9), // "addObject"
+QT_MOC_LITERAL(16, 102, 7), // "Object*"
+QT_MOC_LITERAL(17, 110, 3), // "obj"
+QT_MOC_LITERAL(18, 114, 7), // "doScene"
+QT_MOC_LITERAL(19, 122, 12), // "setLightMove"
+QT_MOC_LITERAL(20, 135, 13), // "setLightAngle"
+QT_MOC_LITERAL(21, 149, 2), // "th"
+QT_MOC_LITERAL(22, 152, 17), // "setLightElevation"
+QT_MOC_LITERAL(23, 170, 3), // "pct"
+QT_MOC_LITERAL(24, 174, 17), // "setLightIntensity"
+QT_MOC_LITERAL(25, 192, 1), // "a"
+QT_MOC_LITERAL(26, 194, 1), // "s"
+QT_MOC_LITERAL(27, 196, 6), // "maxFPS"
+QT_MOC_LITERAL(28, 203, 9), // "addShader"
+QT_MOC_LITERAL(29, 213, 4), // "vert"
+QT_MOC_LITERAL(30, 218, 4), // "frag"
+QT_MOC_LITERAL(31, 223, 5), // "names"
+QT_MOC_LITERAL(32, 229, 10), // "addShader3"
+QT_MOC_LITERAL(33, 240, 4), // "geom"
+QT_MOC_LITERAL(34, 245, 4) // "tick"
+
+    },
+    "CUgl\0light\0\0angle\0angles\0text\0reset\0"
+    "setDim\0d\0setPerspective\0on\0setShader\0"
+    "sel\0setObject\0type\0addObject\0Object*\0"
+    "obj\0doScene\0setLightMove\0setLightAngle\0"
+    "th\0setLightElevation\0pct\0setLightIntensity\0"
+    "a\0s\0maxFPS\0addShader\0vert\0frag\0names\0"
+    "addShader3\0geom\0tick"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_CUgl[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      18,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       2,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags
+       1,    1,  104,    2, 0x06 /* Public */,
+       4,    1,  107,    2, 0x06 /* Public */,
+
+ // slots: name, argc, parameters, tag, flags
+       6,    0,  110,    2, 0x0a /* Public */,
+       7,    1,  111,    2, 0x0a /* Public */,
+       9,    1,  114,    2, 0x0a /* Public */,
+      11,    1,  117,    2, 0x0a /* Public */,
+      13,    1,  120,    2, 0x0a /* Public */,
+      15,    1,  123,    2, 0x0a /* Public */,
+      18,    0,  126,    2, 0x0a /* Public */,
+      19,    1,  127,    2, 0x0a /* Public */,
+      20,    1,  130,    2, 0x0a /* Public */,
+      22,    1,  133,    2, 0x0a /* Public */,
+      24,    3,  136,    2, 0x0a /* Public */,
+      27,    1,  143,    2, 0x0a /* Public */,
+      28,    3,  146,    2, 0x0a /* Public */,
+      28,    2,  153,    2, 0x2a /* Public | MethodCloned */,
+      32,    3,  158,    2, 0x0a /* Public */,
+      34,    0,  165,    2, 0x08 /* Private */,
+
+ // signals: parameters
+    QMetaType::Void, QMetaType::Int,    3,
+    QMetaType::Void, QMetaType::QString,    5,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Float,    8,
+    QMetaType::Void, QMetaType::Int,   10,
+    QMetaType::Void, QMetaType::Int,   12,
+    QMetaType::Void, QMetaType::Int,   14,
+    QMetaType::Void, 0x80000000 | 16,   17,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Bool,   10,
+    QMetaType::Void, QMetaType::Int,   21,
+    QMetaType::Void, QMetaType::Int,   23,
+    QMetaType::Void, QMetaType::Float, QMetaType::Float, QMetaType::Float,   25,    8,   26,
+    QMetaType::Void, QMetaType::Bool,    2,
+    QMetaType::Void, QMetaType::QString, QMetaType::QString, QMetaType::QString,   29,   30,   31,
+    QMetaType::Void, QMetaType::QString, QMetaType::QString,   29,   30,
+    QMetaType::Void, QMetaType::QString, QMetaType::QString, QMetaType::QString,   29,   33,   30,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void CUgl::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        CUgl *_t = static_cast<CUgl *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->light((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 1: _t->angles((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 2: _t->reset(); break;
+        case 3: _t->setDim((*reinterpret_cast< float(*)>(_a[1]))); break;
+        case 4: _t->setPerspective((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 5: _t->setShader((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 6: _t->setObject((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 7: _t->addObject((*reinterpret_cast< Object*(*)>(_a[1]))); break;
+        case 8: _t->doScene(); break;
+        case 9: _t->setLightMove((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 10: _t->setLightAngle((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 11: _t->setLightElevation((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 12: _t->setLightIntensity((*reinterpret_cast< float(*)>(_a[1])),(*reinterpret_cast< float(*)>(_a[2])),(*reinterpret_cast< float(*)>(_a[3]))); break;
+        case 13: _t->maxFPS((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 14: _t->addShader((*reinterpret_cast< QString(*)>(_a[1])),(*reinterpret_cast< QString(*)>(_a[2])),(*reinterpret_cast< QString(*)>(_a[3]))); break;
+        case 15: _t->addShader((*reinterpret_cast< QString(*)>(_a[1])),(*reinterpret_cast< QString(*)>(_a[2]))); break;
+        case 16: _t->addShader3((*reinterpret_cast< QString(*)>(_a[1])),(*reinterpret_cast< QString(*)>(_a[2])),(*reinterpret_cast< QString(*)>(_a[3]))); break;
+        case 17: _t->tick(); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        void **func = reinterpret_cast<void **>(_a[1]);
+        {
+            typedef void (CUgl::*_t)(int );
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&CUgl::light)) {
+                *result = 0;
+                return;
+            }
+        }
+        {
+            typedef void (CUgl::*_t)(QString );
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&CUgl::angles)) {
+                *result = 1;
+                return;
+            }
+        }
+    }
+}
+
+const QMetaObject CUgl::staticMetaObject = {
+    { &QOpenGLWidget::staticMetaObject, qt_meta_stringdata_CUgl.data,
+      qt_meta_data_CUgl,  qt_static_metacall, Q_NULLPTR, Q_NULLPTR}
+};
+
+
+const QMetaObject *CUgl::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *CUgl::qt_metacast(const char *_clname)
+{
+    if (!_clname) return Q_NULLPTR;
+    if (!strcmp(_clname, qt_meta_stringdata_CUgl.stringdata0))
+        return static_cast<void*>(const_cast< CUgl*>(this));
+    return QOpenGLWidget::qt_metacast(_clname);
+}
+
+int CUgl::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QOpenGLWidget::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 18)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 18;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 18)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 18;
+    }
+    return _id;
+}
+
+// SIGNAL 0
+void CUgl::light(int _t1)
+{
+    void *_a[] = { Q_NULLPTR, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
+    QMetaObject::activate(this, &staticMetaObject, 0, _a);
+}
+
+// SIGNAL 1
+void CUgl::angles(QString _t1)
+{
+    void *_a[] = { Q_NULLPTR, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
+    QMetaObject::activate(this, &staticMetaObject, 1, _a);
+}
+QT_END_MOC_NAMESPACE
